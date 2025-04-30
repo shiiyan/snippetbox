30e158d6fe78cd2b0dca8a5c6fba2db0c5e5ed85
shiiyan <shiiyan79@gmail.com>
2025-04-30 12:36:33 +0900

    feat: add config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log/slog"
 	"net/http"
 	"os"
@@ -8,20 +9,33 @@ import (
 
 type application struct {
 	logger *slog.Logger
+	config *config
+}
+
+type config struct {
+	addr      string
+	staticDir string
 }
 
 func main() {
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+
+	flag.Parse()
+
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
 	}))
 
 	app := &application{
 		logger: logger,
+		config: &cfg,
 	}
 
-	logger.Info("starting server", slog.String("addr", ":4000"))
+	logger.Info("starting server", slog.String("addr", cfg.addr))
 
-	err := http.ListenAndServe(":4000", app.routes())
+	err := http.ListenAndServe(cfg.addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -5,7 +5,7 @@ import "net/http"
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(app.config.staticDir))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", app.home)
