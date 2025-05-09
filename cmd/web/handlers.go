70b514955d8239ce1696248f6210aaa3f31bcbd7
shiiyan <shiiyan79@gmail.com>
2025-05-09 21:54:09 +0900

    feat: add handlers for user signup login and logout

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -95,3 +95,19 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
+
+func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+
+}
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+
+}
